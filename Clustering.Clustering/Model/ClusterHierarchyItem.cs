1f8473b0e4cc3cf453a8833d0493889897fce5a9
SOLARIS\Anton <anton.baula@gmail.com>
2018-02-28 19:37:02 +0300

    Заготовка иерархии кластеров.

---
+++ Clustering.Clustering/Model/ClusterHierarchyItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Clustering.Clustering.Model
+{
+    public class ClusterHierarchyItem
+    {
+        public int Level { get; set; }
+        public ClusterHierarchyItem Parent { get; set; }
+        public ClusterHierarchyItem[] Children { get; set; }
+        public Cluster Cluster { get; set; }
+    }
+}
